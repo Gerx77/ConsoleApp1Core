42bc2d73e942f866edefb1a46f562b8a657529ea
matt.gercovitch <matt.gercovitch@gmail.com>
2021-03-06 21:05:25 +1100

    first commit

---
+++ ConsoleApp1Core/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Matt!");
+
+            //Console.ReadLine();
+        }
+    }
+}
